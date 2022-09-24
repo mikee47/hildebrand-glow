615ec606708cf3964715bf5bea1e4c418dd43f92
mikee47 <mike@sillyhouse.net>
2022-09-24 14:34:29 +0100

    Fetch historical data

--- glow.py
+++ glow.py
@@ -8,6 +8,9 @@ client = BrightClient()
 client.token = config["token"]
 virtualEntities = client.get_virtual_entities()
 
+electricity_data = open("electricity.txt", "a")
+gas_data = open("gas.txt", "a")
+
 for ve in virtualEntities.values():
     print(f"Virtual Entity '{ve.name}'")
     print(json.dumps(ve.data, indent=2))
@@ -26,12 +29,17 @@ for ve in virtualEntities.values():
     # t_to = t_from + datetime.timedelta(hours=23, minutes=59)
 
     # t_to = datetime.datetime.now().replace(hour=23, minute=59, second=0)
-    t_to = datetime.datetime(2022, 1, 1)
-    t_from = t_to - datetime.timedelta(days=10)
+    t_from = datetime.datetime(2021, 8, 1)
+    t_end = datetime.datetime(2022, 9, 25)
 
-    readings = resources["electricity consumption"].get_readings(t_from, t_to)
-    for reading in readings:
-        print(reading)
-    readings = resources["gas consumption"].get_readings(t_from, t_to)
-    for reading in readings:
-        print(reading)
+    while t_from < t_end:
+        t_to = t_from + datetime.timedelta(days=7) - datetime.timedelta(seconds=1)
+
+        readings = resources["electricity consumption"].get_readings(t_from, t_to)
+        for reading in readings:
+            print(reading, file=electricity_data)
+        readings = resources["gas consumption"].get_readings(t_from, t_to)
+        for reading in readings:
+            print(reading, file=gas_data)
+
+        t_from += datetime.timedelta(days=7)
